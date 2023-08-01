cd44d427fcb71d975685fb2b2197371dc93bb13e
Jaga1999 <jagasiva1999@gmail.com>
2023-08-01 10:24:03 +0530

    added another get route

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 
-    app.Listen(":3000")
+	app.Get("/all", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, All!")
+	})
+
+	app.Listen(":3000")
 }
